1f2c2c6521c8b2fe02b6ce3f3e7084a071301fd2
gt005 <karimhamid@yandex.ru>
2023-10-14 19:09:25 +0000

    Add models to admin panel

--- backend/project/apps/nearest_bank_api/admin.py
+++ backend/project/apps/nearest_bank_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from project.apps.nearest_bank_api.models import Service, SalePoint, Atm, Ticket
 
-# Register your models here.
+admin.site.register(Service)
+admin.site.register(SalePoint)
+admin.site.register(Atm)
+admin.site.register(Ticket)
