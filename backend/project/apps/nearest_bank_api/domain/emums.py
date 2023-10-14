0e1bf2f76b051e5d258b848898c4eb9028f6a4aa
gt005 <karimhamid@yandex.ru>
2023-10-14 19:07:05 +0000

    Chg: Replaced english service names by russian names

--- backend/project/apps/nearest_bank_api/domain/emums.py
+++ backend/project/apps/nearest_bank_api/domain/emums.py
@@ -2,23 +2,30 @@ from enum import Enum
 
 
 class ServiceNameEnum(Enum):
-    supportsEur = "supportsEur"
-    supportsRub = "supportsRub"
-    supportsUsd = "supportsUsd"
-    supportsChargeRub = "supportsChargeRub"
-    blind = "blind"
-    nfcForBankCards = "nfcForBankCards"
-    qrRead = "qrRead"
-    wheelchair = "wheelchair"
+    supportsEur = ('Работа с Евро')
+    supportsRub = ('Работа с Рублями')
+    supportsUsd = ('Работа с Долларами')
+    supportsChargeRub = ('Поддержка зарядки рублей')
+    blind = ('Услуги для слепых')
+    nfcForBankCards = ('Поддержка NFC для банковских карт')
+    qrRead = ('Чтение QR-кодов')
+    wheelchair = ('Доступность для инвалидных колясок')
 
+    @property
+    def eng(self):
+        return self.value[0]
+
+    @property
+    def ru(self):
+        return self.value[1]
 
 class ServiceCapabilityEnum(Enum):
-    SUPPORTED = "SUPPORTED"
-    UNKNOWN = "UNKNOWN"
-    UNSUPPORTED = "UNSUPPORTED"
+    SUPPORTED = 'SUPPORTED'
+    UNKNOWN = 'UNKNOWN'
+    UNSUPPORTED = 'UNSUPPORTED'
 
 
 class ServiceActivityEnum(Enum):
-    AVAILABLE = "AVAILABLE"
-    UNAVAILABLE = "UNAVAILABLE"
-    UNKNOWN = "UNKNOWN"
+    AVAILABLE = 'AVAILABLE'
+    UNAVAILABLE = 'UNAVAILABLE'
+    UNKNOWN = 'UNKNOWN'
