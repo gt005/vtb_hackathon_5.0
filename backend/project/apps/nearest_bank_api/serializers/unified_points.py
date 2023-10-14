89ade65f0a7d74f222b8f06a17f5741142d343f9
gt005 <karimhamid@yandex.ru>
2023-10-14 21:53:49 +0000

    Add open hours to sales point api

--- backend/project/apps/nearest_bank_api/serializers/unified_points.py
+++ backend/project/apps/nearest_bank_api/serializers/unified_points.py
@@ -11,6 +11,7 @@ from project.apps.nearest_bank_api.selectors.unified_points import (
     unified_point_get_service_id_list,
 )
 from project.apps.nearest_bank_api.serializers.ticket import TicketReadSerializer
+from project.apps.nearest_bank_api.serializers.sale_point import OpenHoursCreateSerializer
 
 
 class UnifiedPoinsReadSerializer(Serializer):
@@ -23,6 +24,7 @@ class UnifiedPoinsReadSerializer(Serializer):
     services = SerializerMethodField()
     serviceStaffAmount = IntegerField(default=None)
     tickets = SerializerMethodField()
+    openHours = SerializerMethodField()
 
     def get_type(self, obj: Atm | SalePoint) -> str:
         if isinstance(obj, Atm):
@@ -38,5 +40,11 @@ class UnifiedPoinsReadSerializer(Serializer):
     def get_tickets(self, obj: Atm | SalePoint) -> list[dict] | None:
         if isinstance(obj, SalePoint):
             return TicketReadSerializer(obj.tickets, many=True).data
+
+        return None
+
+    def get_openHours(self, obj: Atm | SalePoint) -> list[dict] | None:
+        if isinstance(obj, SalePoint):
+            return OpenHoursCreateSerializer(obj.openHours.all(), many=True).data
 
         return None
